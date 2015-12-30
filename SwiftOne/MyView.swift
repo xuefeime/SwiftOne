ff26fad2e8061d815777d0348cda69c0c22cb7a7
王雪飞 <792519422@qq.com>
2015-12-30 16:31:11 +0800

    Revert "w"
    This reverts commit fbf60db0980e4027d8ddb1ac5dde151de0d08a00.

--- SwiftOne/MyView.swift
+++ SwiftOne/MyView.swift
@@ -10,18 +10,12 @@ import UIKit
 
 class MyView: UIView {
 
-    var title:String = "one"
-    var titleTwo:String = "two"
-    var nameLabel:UILabel = UILabel()
+    /*
     // Only override drawRect: if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func drawRect(rect: CGRect) {
         // Drawing code
-        nameLabel.frame = CGRectMake(rect.origin.x+10, rect.origin.y+10, rect.size.width-20, rect.size.height-20)
-        nameLabel.text = title;
-        nameLabel.backgroundColor = UIColor.greenColor()
-        self.addSubview(nameLabel);
     }
-
+    */
 
 }
