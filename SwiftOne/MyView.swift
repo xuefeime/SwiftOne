1b397f5cc84f7f75af541d5e580fb1f88ad00a33
王雪飞 <792519422@qq.com>
2015-12-30 16:01:29 +0800

    hehe

---
+++ SwiftOne/MyView.swift
@@ -0,0 +1,21 @@
+//
+//  MyView.swift
+//  SwiftOne
+//
+//  Created by 赵国栋 on 15/12/30.
+//  Copyright © 2015年 王雪飞. All rights reserved.
+//
+
+import UIKit
+
+class MyView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
