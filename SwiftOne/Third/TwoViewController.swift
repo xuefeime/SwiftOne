86b903c2e404a2089f136bcb142f9ad5e06cf781
王雪飞 <792519422@qq.com>
2016-02-16 14:15:40 +0800

    tangy two

---
+++ SwiftOne/Third/TwoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TwoViewController.swift
+//  SwiftOne
+//
+//  Created by 赵国栋 on 16/2/16.
+//  Copyright © 2016年 王雪飞. All rights reserved.
+//
+
+import UIKit
+
+class TwoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
